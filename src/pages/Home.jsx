b44e131aa641b6f42a3a0c263d8b128eefd1fff0
emansuliman4 <emanmagdyahmed445@gmail.com>
2025-10-07 04:06:18 +0300

    add some icons and animation to start new section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import eman from "../images/eman1.jpeg";
+import Social from "@/components/Social";
+import { Heart, Star } from "@/components/Icons";
 
-export default function Home() {
+function HomeComponent() {
   return (
-    <div className="p-4">
+    <div className="h-screen flex justify-center p-4">
       <div className="w-80 h-75 mx-auto bg-black rounded-[50%] relative font-mono">
         <img
           src={eman}
           alt="Eman Suliman"
-          className="rounded-[50%] w-64 h-64 mx-auto w-80 h-70 animate-wave "
+          className="rounded-[50%] mx-auto w-80 h-70 animate-wave"
         />
 
         <div className="animate-bounce">
@@ -62,10 +64,59 @@ export default function Home() {
             <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
           </svg>
         </div>
-        <p className="font-bold text-white bg-black rounded-[20%] h-20 w-72 sm:w-80 animate-waveText text-4xl sm:text-5xl flex justify-center items-center whitespace-nowrap absolute -bottom-50 ">
+        <p className=" text-white bg-black rounded-[20%] h-20 w-72 sm:w-80 animate-waveText font-bold text-4xl sm:text-5xl flex justify-center items-center whitespace-nowrap absolute -bottom-50 ">
           DEVELOPER
         </p>
       </div>
+    </div>
+  );
+}
+
+function IconNewPage() {
+  return (
+    <div className="bg-black mt-50 ">
+      <div className="flex justify-center ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="50"
+          height="60"
+          viewBox="0 0 24 24"
+          fill="white"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-circle-chevron-down-icon lucide-circle-chevron-down relative z-10 bottom-6 animate-bounce "
+        >
+          <circle cx="12" cy="12" r="10" />
+          <path d="m16 10-4 4-4-4" />
+        </svg>
+      </div>
+      <svg viewBox="0 0 1200 100" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0,50 Q300,20 600,50 T1200,50 L1200,0 L0,0 Z" fill="white" />
+      </svg>
+      <Star />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 100">
+        <path
+          fill="white"
+          fill-opacity="1"
+          d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+      <div className="flex justify-around items-center ">
+        <Heart />
+        <Star />
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className="overflow-x-hidden ">
+      <HomeComponent />;
+      <Social />
+      <IconNewPage />
     </div>
   );
 }
