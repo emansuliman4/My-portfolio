b780ceb30ffd0f4b83566b8b579e8bc9592cfcab
emansuliman4 <emanmagdyahmed445@gmail.com>
2025-10-19 20:52:36 +0300

    edit config file to build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
+import { resolve } from "path";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -10,6 +11,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "./",
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public",
+  assetsInclude: ["_redirects"],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
